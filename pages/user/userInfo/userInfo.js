3b47c50c862c4b3639658a304f2bd66121a6ef49
shaohua.wsh <shaohuawang2015@163.com>
2017-07-28 08:39:45 +0800

    modify apps

--- pages/user/userInfo/userInfo.js
+++ pages/user/userInfo/userInfo.js
@@ -1,66 +1,93 @@
 // userInfo.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        userInfo: {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+        },
+        taskInfo: [{
+            'id': 'personalInfo',
+            'desc': '我的信息'
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+        }, {
+            'id': 'level',
+            'desc': '我的等级'
+        }, {
+            'id': 'harvest',
+            'desc': '我的成果'
+        }],
+        orderInfo: [{
+            'id': 'history',
+            'desc': '历史订单'
+        },{
+            'id': 'favorite',
+            'desc': '收藏栏目'
+        }],
+        otherInfo: [{
+            'id': 'version',
+            'desc': '版本信息'
+        },{
+            'id': 'contact',
+            'desc': '联系我们'
+        }]
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
